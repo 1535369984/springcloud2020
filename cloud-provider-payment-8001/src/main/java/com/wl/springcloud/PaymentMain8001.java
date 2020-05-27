6f1ee18450b46c65ecf5f7e5a6f28594faab9084
wanglei <1535369984@qq.com>
2020-05-27 23:52:50 +0800

    optimise

---
+++ cloud-provider-payment-8001/src/main/java/com/wl/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.wl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wl
+ * @description
+ * @date 2020/5/27 23:34
+ * @tel 18708125827
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args){
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
